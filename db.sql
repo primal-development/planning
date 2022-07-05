4799a6f7c74f8fb04fd519ea866b8739c6f309b1
Silas Demez <silasdemez@gmail.com>
2022-07-05 10:57:58 +0200

    Update db.sql

--- db.sql
+++ db.sql
@@ -4,6 +4,7 @@ DROP TABLE IF EXISTS `Trainer`;
 DROP TABLE IF EXISTS `PlannedActivity`;
 DROP TABLE IF EXISTS `Activity`;
 DROP TABLE IF EXISTS `athlete_trainer`;
+DROP TABLE IF EXISTS `Sport`;
 SET FOREIGN_KEY_CHECKS = 1;
 
 CREATE TABLE `Athlete` (
@@ -29,9 +30,11 @@ CREATE TABLE `PlannedActivity` (
     `activity_name` VARCHAR(30) NOT NULL,
     `activity_description` VARCHAR(80),
     `activity_date` DATE,
-    `time_of_day` varchar(1),
+    `time_of_day` VARCHAR(1),
     `activity_img` BLOB,
     `athlete_id` INTEGER NOT NULL,
+    `duration` TIMESTAMP NOT NULL,
+    `sport_id` INTEGER NOT NULL,
     PRIMARY KEY (`activity_id`)
 );
 
@@ -39,9 +42,13 @@ CREATE TABLE `Activity` (
     `activity_id` INTEGER NOT NULL AUTO_INCREMENT,
     `activity_name` VARCHAR(30) NOT NULL,
     `activity_description` VARCHAR(80),
-    `activity_location` POINT,
     `activity_time` DATETIME NOT NULL,
     `athlete_id` INTEGER NOT NULL,
+    `duration` TIMESTAMP NOT NULL,
+    `distance` DOUBLE,
+    `sport_id` INTEGER NOT NULL,
+    `elevation` INTEGER,
+    `avg_velocity` VARCHAR(10),
     PRIMARY KEY (`activity_id`)
 );
 
@@ -51,7 +58,15 @@ CREATE TABLE `athlete_trainer` (
     PRIMARY KEY (`athlete_id`, `trainer_id`)
 );
 
+CREATE TABLE `Sport` (
+    `sport_id` INTEGER NOT NULL AUTO_INCREMENT,
+    `sport_name` VARCHAR(30) NOT NULL,
+    PRIMARY KEY (`sport_id`)
+);
+
 ALTER TABLE `PlannedActivity` ADD FOREIGN KEY (`athlete_id`) REFERENCES `Athlete`(`athlete_id`);
+ALTER TABLE `PlannedActivity` ADD FOREIGN KEY (`sport_id`) REFERENCES `Sport`(`sport_id`);
 ALTER TABLE `Activity` ADD FOREIGN KEY (`athlete_id`) REFERENCES `Athlete`(`athlete_id`);
+ALTER TABLE `Activity` ADD FOREIGN KEY (`sport_id`) REFERENCES `Sport`(`sport_id`);
 ALTER TABLE `athlete_trainer` ADD FOREIGN KEY (`athlete_id`) REFERENCES `Athlete`(`athlete_id`);
 ALTER TABLE `athlete_trainer` ADD FOREIGN KEY (`trainer_id`) REFERENCES `Trainer`(`trainer_id`);
