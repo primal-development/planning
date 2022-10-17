41405330f7fc6cedfb0baa293ec3ee9222932cb6
Silas Demez <silasdemez@gmail.com>
2022-10-17 11:47:52 +0200

    change db structure

--- db.sql
+++ db.sql
@@ -6,14 +6,15 @@ DROP TABLE IF EXISTS `group`;
 DROP TABLE IF EXISTS `Sport`;
 DROP TABLE IF EXISTS `athlete_plannedActivity`;
 DROP TABLE IF EXISTS `presences`;
+DROP TABLE IF EXISTS `group_athlete`;
 SET FOREIGN_KEY_CHECKS = 1;
 
 CREATE TABLE `Athlete` (
     `athlete_id` INTEGER NOT NULL,
     `athlete_name` VARCHAR(30) NOT NULL,
     `athlete_lastname` VARCHAR(30) NOT NULL,
+    `email_address` VARCHAR(30) NOT NULL,
     `pass_hash` VARCHAR(300) NOT NULL,
-    `athlete_salt` VARCHAR(10),
     `refresh_token` VARCHAR(50),
     PRIMARY KEY (`athlete_id`)
 );
@@ -27,16 +28,16 @@ CREATE TABLE `PlannedActivity` (
     `time_of_day` VARCHAR(1),
     `activity_img` BLOB,
     `sport_id` INTEGER NOT NULL,
-    `L1` DOUBLE,
-    `L2` DOUBLE,
-    `L3` DOUBLE,
-    `L4` DOUBLE,
-    `L5` DOUBLE,
-    `core` DOUBLE,
-    `arms` DOUBLE,
-    `legs` DOUBLE,
-    `agility` DOUBLE,
-    `technique` DOUBLE,
+    `L1` TIME,
+    `L2` TIME,
+    `L3` TIME,
+    `L4` TIME,
+    `L5` TIME,
+    `core` TIME,
+    `arms` TIME,
+    `legs` TIME,
+    `agility` TIME,
+    `technique` TIME,
     PRIMARY KEY (`activity_id`)
 );
 
@@ -51,24 +52,24 @@ CREATE TABLE `Activity` (
     `avg_velocity` VARCHAR(10),
     `athlete_id` INTEGER NOT NULL,
     `sport_id` INTEGER NOT NULL,
-    `L1` DOUBLE,
-    `L2` DOUBLE,
-    `L3` DOUBLE,
-    `L4` DOUBLE,
-    `L5` DOUBLE,
-    `core` DOUBLE,
-    `arms` DOUBLE,
-    `legs` DOUBLE,
-    `agility` DOUBLE,
-    `technique` DOUBLE,
+    `L1` TIME,
+    `L2` TIME,
+    `L3` TIME,
+    `L4` TIME,
+    `L5` TIME,
+    `core` TIME,
+    `arms` TIME,
+    `legs` TEXT,
+    `agility` TIME,
+    `technique` TIME,
     PRIMARY KEY (`activity_id`)
 );
 
 CREATE TABLE `group` (
+    `group_id` INTEGER NOT NULL,
     `group_name` VARCHAR(30) NOT NULL,
-    `athlete_id` INTEGER NOT NULL,
     `trainer_id` INTEGER NOT NULL,
-    PRIMARY KEY (`group_name`)
+    PRIMARY KEY (`group_id`)
 );
 
 CREATE TABLE `Sport` (
@@ -90,12 +91,19 @@ CREATE TABLE `presences` (
     PRIMARY KEY (`athlete_id`, `activity_id`)
 );
 
+CREATE TABLE `group_athlete` (
+    `group_id` INTEGER NOT NULL,
+    `athlete_id` INTEGER NOT NULL,
+    PRIMARY KEY (`group_id`, `athlete_id`)
+);
+
 ALTER TABLE `PlannedActivity` ADD FOREIGN KEY (`sport_id`) REFERENCES `Sport`(`sport_id`);
 ALTER TABLE `Activity` ADD FOREIGN KEY (`athlete_id`) REFERENCES `Athlete`(`athlete_id`);
 ALTER TABLE `Activity` ADD FOREIGN KEY (`sport_id`) REFERENCES `Sport`(`sport_id`);
-ALTER TABLE `group` ADD FOREIGN KEY (`athlete_id`) REFERENCES `Athlete`(`athlete_id`);
 ALTER TABLE `group` ADD FOREIGN KEY (`trainer_id`) REFERENCES `Athlete`(`athlete_id`);
 ALTER TABLE `athlete_plannedActivity` ADD FOREIGN KEY (`athlete_id`) REFERENCES `Athlete`(`athlete_id`);
 ALTER TABLE `athlete_plannedActivity` ADD FOREIGN KEY (`activity_id`) REFERENCES `PlannedActivity`(`activity_id`);
 ALTER TABLE `presences` ADD FOREIGN KEY (`athlete_id`) REFERENCES `Athlete`(`athlete_id`);
 ALTER TABLE `presences` ADD FOREIGN KEY (`activity_id`) REFERENCES `PlannedActivity`(`activity_id`);
+ALTER TABLE `group_athlete` ADD FOREIGN KEY (`athlete_id`) REFERENCES `Athlete`(`athlete_id`);
+ALTER TABLE `group_athlete` ADD FOREIGN KEY (`group_id`) REFERENCES `group`(`group_id`);
